30a0a5a06f8dec3427b3dce0f36dd3d72799bb6d
Hayden Bleasel <hello@haydenbleasel.com>
2023-02-12 23:57:44 -0500

    Add metadata

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 import Seo from '@/components/seo';
 
-const Head: FC = () => <Seo />;
+const Head: FC = () => <Seo title="Games" description="My Steam game stats" />;
 
 export default Head;
